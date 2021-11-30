ba884c65020560cd11964f369b15ae97bb6b7b84
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-11-29 17:44:08 -0800

    Create Card class, create Turn class, create Turn-test

---
+++ test/Turn-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', function(){
+
+    it('should be a function', function(){
+        const turn = new Turn();
+        expect(Turn).to.be.a('function');
+    })
+
+    it('should be an instance of Card', function(){
+        const turn = new Turn();
+        expect(turn).to.be.an.instanceof(Turn);
+    })
+    
+});
